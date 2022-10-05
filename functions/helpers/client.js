e5afa43533b141032521af2f7c529b4da5b5fcfb
Fyodor <sivkov1998@yandex.ru>
2022-10-05 12:22:12 +0300

    connection bug

--- functions/helpers/client.js
+++ functions/helpers/client.js
@@ -1,9 +1,14 @@
 const Client = require('pg').Client
 
 const pool = new Client({
-  connectionString: process.env.DB_CONNECTION_STRING,
+  // connectionString: process.env.DB_CONNECTION_STRING,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: 5432,
   ssl: { rejectUnauthorized: false }
-})
+}) 
 pool.connect()
 
 module.exports = pool
